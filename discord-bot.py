d5d339d132306b057f3fc72849ec9ef40fa86393
Adam Wilson <point5r@gmail.com>
2021-03-04 15:32:07 +0000

    add stuff

--- discord-bot.py
+++ discord-bot.py
@@ -1,24 +1,36 @@
 #!/usr/bin/env python3
 from filelock import FileLock
 from systemd import service
+import os
+from tempfile import gettempdir
 
 VALHEIM_SERVICE = "bluetooth"
 
 
-def create_lock(locktype: str) -> FileLock:
-    lock_path = locktype + ".lock"
+def create_lock(lock_type: str) -> FileLock:
+    lock_path = os.path.join(gettempdir(), lock_type + ".lock")
     lock = FileLock(lock_path)
     return lock
 
 
-def main():
+def check_status(serv: service.Service):
     lock = create_lock("status")
     lock.acquire(timeout=5)
-
-    valheim = service.Service(VALHEIM_SERVICE)
-    print(valheim.check_status())
-
+    status = serv.check_status()
+    print(status)
     lock.release()
+
+def restart_service(serv: service.Service):
+    lock = create_lock("restart")
+    lock.acquire(timeout=5)
+    status = serv.restart()
+
+
+def main():
+    valheim = service.Service(VALHEIM_SERVICE)
+    check_status(valheim)
+    restart_service(valheim)
+
 
 if __name__ == "__main__":
     main()
--- systemd/service.py
+++ systemd/service.py
@@ -5,8 +5,35 @@ import subprocess
 
 class Service:
     def __init__(self, name):
-        self.name = name+".service"
+        self.service_name = name + ".service"
+        self.name = name
 
-    def check_status(self):
-        cmd = subprocess.run(f"systemctl status {self.name}", capture_output=True, text=True)
-        print(cmd.stdout)
+    def __build_command(self, cmd: str) -> str:
+        return f"systemctl {cmd} {self.service_name}"
+
+    @staticmethod
+    def __sub_run(cmd: str):
+        return subprocess.run(cmd.split(),
+                              shell=True, capture_output=True, text=True)
+
+    @staticmethod
+    def __sub_check_output(cmd: str):
+        return subprocess.check_output(cmd.split())
+
+    def restart(self):
+        cmd = self.__build_command("restart")
+        out = self.__sub_check_output(cmd)
+        print(out)
+
+    def check_status(self) -> str:
+
+        cmd = self.__build_command("status")
+        run = self.__sub_check_output(cmd)
+
+        out = run.decode()
+        out_str = out.split()
+        i = 0
+        while i < len(out_str):
+            if out_str[i] == "Active:":
+                return f"The {self.name} service is {out_str[i + 1]}"
+            i += 1
